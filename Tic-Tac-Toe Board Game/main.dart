b7e098cb93952a3c75a61b66c64230d6c59cd913
hashimsaffarini <hashimsaffarini044@gmail.com>
2023-12-13 23:16:25 +0200

    Board Game

---
+++ Tic-Tac-Toe Board Game/main.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+bool winner = false, flag = true;
+int count = 0;
+List<String> list = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+List<String> comp = ['012', '048', '036', '147', '246', '258', '345', '678'];
+void main(List<String> args) {
+  shape();
+  user();
+}
+
+void shape() {
+  print('${list[0]}  | ${list[1]} | ${list[2]} ');
+  print('---+---+---');
+  print('${list[3]}  | ${list[4]} | ${list[5]} ');
+  print('---+---+---');
+  print('${list[6]}  | ${list[7]} | ${list[8]} ');
+}
+
+void user() {
+  print('Choose Number For ${flag ? 'X' : 'O'}');
+  int num = int.parse(stdin.readLineSync()!);
+  list[num - 1] = flag ? 'X' : 'O';
+  count++;
+  flag = !flag;
+  if (count == 9) {
+    winner=true;
+    print('DRAW ...');
+  } else {
+    shape();
+  }
+  check_winner('X');
+  check_winner('O');
+  if(winner==false){
+    user();
+  }
+}
+
+bool check(String a, String b) {
+  List<int> n = a.split('').map((k) {
+    return int.parse(k);
+  }).toList();
+  for (final item in n) {
+    if (list[item] != b) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+void check_winner(player) {
+  for (final item in comp) {
+    bool c = check(item, player);
+    if(c){
+      winner=true;
+      print('$player WON ....');
+      return;
+    }
+  }
+}
